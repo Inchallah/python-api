397ccf476e5ed4c80cdf2d3a5750978b1adc9481
Inchallah <aurelien.montbas@gmail.com>
2022-10-17 12:07:45 +0200

    /file route done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import socket
 import os
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +14,22 @@ def gepete(name):
 @app.route('/state')
 def status():
     return "Message: " + os.environ.get('MESSAGE') + " | Name: " + os.environ.get('NAME') + " | Sender: " + os.environ.get('SENDER')
+
+
+@app.route('/file')
+def readFromFile():
+    # read from config
+    with open('config.json', 'r') as config:
+        configData = json.loads(config)
+        configStr = "Hello sender: " + configData['sender'] + \
+            ", sent this message to you " + configData['message']
+    with open('secrets.json', 'r') as secrets:
+        secretsData = json.loads(secrets)
+        secretStr = "Hello sender: " + secretsData['sender'] + \
+            ", sent this message to you " + secretsData['message']
+    simpleResponse = configStr + '<br>' + secretStr
+    # read from secret
+    return simpleResponse
 
 
 if __name__ == "__main__":
